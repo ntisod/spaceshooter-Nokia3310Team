dccb44c236756c5a60c718743b5bf9852fb53251
Nokia3310Team <icake.cc@gmail.com>
2019-09-27 14:15:05 +0200

    Update 19/09/27

--- SpaceShooter/Game1.cs
+++ SpaceShooter/Game1.cs
@@ -14,11 +14,10 @@ namespace SpaceShooter
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Vector2 ship_vector;
-        Vector2 ship_speed;
-
-        //variabler
-        Texture2D ship_texture;
+        Player player;
+        Texture2D texture;
+        Vector2 vector;
+        Vector2 speed;
 
         public Game1()
         {
@@ -34,13 +33,7 @@ namespace SpaceShooter
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            ship_vector.X = 380;
-            ship_vector.Y = 400;
-
-            ship_speed.X = 2.5f;
-            ship_speed.Y = 4.5f;
-
+ 
             base.Initialize();
 
         }
@@ -51,48 +44,36 @@ namespace SpaceShooter
         /// </summary>
         protected override void LoadContent()
         {
-            
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-            ship_texture = Content.Load<Texture2D>("Sprites/ship");
+            player = new Player(Content.Load<Texture2D>("Sprites/ship"), 380, 400, 2.5f, 4.5f);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+      
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+            
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
+
+       
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            KeyboardState keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            ship_vector.X += ship_speed.X;
-            if (ship_vector.X > Window.ClientBounds.Width - ship_texture.Width || ship_vector.X < 0)
-            {
-                ship_speed.X *= -1;
-            }
+            player.Update(Window);
 
-            ship_vector.Y += ship_speed.Y;
-            if (ship_vector.Y > Window.ClientBounds.Height - ship_texture.Height || ship_vector.Y < 0)
-            {
-                ship_speed.Y *= -1;
-            }
 
 
             base.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, vector, Color.White);
         }
 
         /// <summary>
@@ -101,11 +82,10 @@ namespace SpaceShooter
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.TransparentBlack);
+            //grafik
             spriteBatch.Begin();
-            spriteBatch.Draw(ship_texture, ship_vector, Color.White);
+            player.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
---
+++ SpaceShooter/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceShooter
+{
+    class Player : MovingObject
+    {
+        public Player(Texture2D texture, float X, float Y, float speedX, float speedY) : base(texture, X, Y, speedX, speedY)
+        {
+            this.texture = texture;
+            this.vector.X = X;
+            this.vector.Y = Y;
+            this.speed.X = speedX;
+            this.speed.Y = speedY;
+        }
+            public void Update(GameWindow window)
+            {
+                KeyboardState keyboardState = Keyboard.GetState();
+
+                if (vector.X <= window.ClientBounds.Width - texture.Width && vector.X >= 0)
+                {
+                    if (keyboardState.IsKeyDown(Keys.Right))
+                        vector.X += speed.X;
+                    if (keyboardState.IsKeyDown(Keys.Left))
+                        vector.X -= speed.X;
+                }
+
+
+                if (vector.Y <= window.ClientBounds.Height - texture.Height && vector.Y >= 0)
+                {
+                    if (keyboardState.IsKeyDown(Keys.Down))
+                        vector.Y += speed.Y;
+                    if (keyboardState.IsKeyDown(Keys.Up))
+                        vector.Y -= speed.Y;
+                }
+
+                if (vector.X < 0)
+                    vector.X = 0;
+
+                if (vector.X > window.ClientBounds.Width - texture.Width)
+                    vector.X = window.ClientBounds.Width - texture.Width;
+
+                if (vector.Y < 0)
+                    vector.Y = 0;
+
+                if (vector.Y > window.ClientBounds.Height - texture.Height)
+                    vector.Y = window.ClientBounds.Height - texture.Height;
+            }
+    }
+
+}
